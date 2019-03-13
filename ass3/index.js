de78cd136a33bfef297dffd3d3deb7da12d1c526
setsal Lan <setsal@csie.io>
2019-03-13 16:00:04 +0800

    Finish basic line read

--- ass3/index.js
+++ ass3/index.js
@@ -3,15 +3,19 @@ var async = require('async')
 var dataSize = 1000
 
 async.series([
+    /* mariadb insert */
     function(callback) {
         require('./mariadb')( 'insert', dataSize, callback )
-    },
-    function(callback) {
-        require('./mongo')( 'insert', dataSize, callback )
-    },
-    function(callback) {
-        require('./elasticsearch')( 'insert', dataSize, callback )
     }
+    //
+    // /* mongodb insert */
+    // function(callback) {
+    //     require('./mongo')( 'insert', dataSize, callback )
+    // },
+    // /* elasticsearch insert */
+    // function(callback) {
+    //     require('./elasticsearch')( 'insert', dataSize, callback )
+    // }
 
 ],function() {
     process.exit()
--- ass3/mariadb.js
+++ ass3/mariadb.js
@@ -1,12 +1,56 @@
 var async = require('async')
-
+var fs = require('fs')
+var readline = require('readline');
 var self = {}
 
 self.insert = function( dataSize, done ) {
     var run = []
-    console.log('mariadb insert start')
+
+	run.push(function(callback){
+        var inputStream = fs.createReadStream('./data/youtube.rec.100000');
+        // 將讀取資料流導入 Readline 進行處理
+        var lineReader = readline.createInterface({ input: inputStream });
+        var count = 6
+        var testfunction = function() {
+            var count = 6
+            lineReader.on('line', function(line) {
+                console.log(line)
+                count = count - 1;
+                if ( count == 0 ) {
+                    return;
+                }
+            })
+        }
+        testfunction()
+        // lineReader.on('line', function(line) {
+        //     // 取得一行行結果
+        //     if ( /^\s*$/.test(line) ){
+        //         return;
+        //     }
+        //     if (line.match(/(@url:)(.+)$/)){
+        //         console.log
+        //     }
+        //     else if ( line.match(/(@title:)(.+)$/) ){
+        //
+        //     }
+        //     else if ( line.match(/(@content:)(.+)$/) ){
+        //
+        //     }
+        //     else if ( line.match(/(@viewCount:)(.+)$/) ){
+        //
+        //     }
+        //     else if ( line.match(/(@res:)(.+)$/) ){
+        //
+        //     }
+        //     else if ( line.match(/(@duration:)(.+)$/) ){
+        //
+        //     }
+        //     console.log(line);
+    });
+
+    console.time('mariadb insert')
     async.series(run, function(err,data){
-        console.log('mariadb insert end')
+        console.timeEnd('mariadb insert')
         if(done)
             done();
     })
