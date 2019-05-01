ac6799488ef32e79324d96feb973548fbf830580
setsal <langeniustw@gmail.com>
2019-05-01 17:58:19 +0800

    Create default data structure in base block

--- ass3/main.c
+++ ass3/main.c
@@ -4,36 +4,69 @@
 #include <stdlib.h>
 
 struct Block {
-
+    unsigned char *blockptr;
+    int blockSize;
 };
 
-struct Base {
-    // int *ptr = malloc(sizeof(int));
-};
 
-unsigned char bytes[BASE_SIZE];
+/*
+    key  \0  id  \0
+    11 + 1 + 4 + 1
+*/
+unsigned char base[BASE_SIZE];
+unsigned char bytes[4];
+long count = 1;
 
-int main () {
-    // int *blockptr = malloc(BLOCK_SIZE);
-    // int *baseptr = malloc(BASE_SIZE);
-    // int count = 1;
 
-    // printf("%d\n", sizeof(char));
-    // int n = 12345;
-    unsigned char bytes[17];
-    unsigned int n = 1;
+void *IntToByte( long value ) {
 
-    bytes[0] = (n >> 24) & 0xFF;
-    bytes[1] = (n >> 16) & 0xFF;
-    bytes[2] = (n >> 8) & 0xFF;
-    bytes[3] = n & 0xFF;
-    printf("%x %x %x %x\n", bytes[0], bytes[1], bytes[2], bytes[3]);
-    // char* a = (char*)&n;
-    // printf("%d")
-    // free(ptr);
-    return 0;
+    bytes[0] = (int)((value >> 24) & 0xFF);
+    bytes[1] = (int)((value >> 16) & 0xFF);
+    bytes[2] = (int)((value >> 8) & 0xFF);
+    bytes[3] = (int)(value & 0xFF);
+
 }
 
-void IntTobinary ( long value ) {
 
+void InsertToBase( unsigned char input[] ) {
+    int i = 0;
+
+    // Key value
+    for( i=0; i<11; i++ ) {
+        base[i] = input[i];
+    }
+
+    base[11] = '\0';
+
+    // ID
+    base[12] = bytes[0];
+    base[13] = bytes[1];
+    base[14] = bytes[2];
+    base[15] = bytes[3];
+
+    base[16] = '\0';
+}
+
+
+
+int main () {
+
+    unsigned char fakeinput[] = { "d4gKFHj4WdI" };
+
+
+    IntToByte( count );
+    InsertToBase( fakeinput );
+
+    struct block tmp;
+    tmp.blockptr = malloc(BLOCK_SIZE);
+
+
+
+    FILE *pFile;
+    pFile = fopen("../data/test.txt","wb");
+    fwrite( &base, sizeof(char), 17,  pFile );
+    fclose(pFile);
+
+
+    return 0;
 }
